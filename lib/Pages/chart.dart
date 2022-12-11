def716698c16c7b2218a4bbaaa5b0d8ebb8c5049
aadil <aadilhasan9697@gmail.com>
2022-12-11 11:08:33 +0530

    Upadte

---
+++ lib/Pages/chart.dart
@@ -0,0 +1,115 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../model.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+
+  Chart(this.recentTransaction);
+
+  List<Map<String, dynamic>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekday = DateTime.now().subtract(Duration(days: index));
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransaction.length; i++) {
+        if (recentTransaction[i].date.day == weekday.day &&
+            recentTransaction[i].date.month == weekday.month &&
+            recentTransaction[i].date.year == weekday.year) {
+          totalSum += recentTransaction[i].amount;
+        }
+      }
+      return {
+        'day': DateFormat.E().format(weekday).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(
+        0.0, (sum, item) => sum + item['amount']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            ...groupedTransactionValues.map((data) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(data['day'], data['amount'],
+                    totalSpending == 0.0 ? 0.0 : data['amount'] / totalSpending
+                ),
+              );
+            }).toList()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/////#############################CHARTBAR########?///////////////////////////////##$$%$
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
+        children: [
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+                child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1.0),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                FractionallySizedBox(
+                  heightFactor: spendingPctOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+              height: constraints.maxHeight * 0.15,
+              child: Text(label))
+        ],
+      );
+    });
+  }
+}
